1e43cb3a4d70980569515947405d2de1ea5c3c32
Divyanshu Shekhar <divyanshushekhar05@gmail.com>
2026-09-25 23:30:15 +0530

    Time: 1 ms (99.81%) | Memory: 42.7 MB (42.77%) - LeetSync

--- 69-sqrtx/sqrtx.java
+++ 69-sqrtx/sqrtx.java
@@ -4,20 +4,21 @@ class Solution {
             return x;
         }
 
-        int low = 1;
-        int high = x;
-        int ans = 0;
+        long left = 1;
+        long right = x / 2;
 
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-            if (mid <= x / mid) {
-                ans = mid;
-                low = mid + 1;
+        while (left <= right) {
+            long mid = left + (right - left) / 2;
+
+            if (mid * mid == x) {
+                return (int) mid;
+            } else if (mid * mid < x) {
+                left = mid + 1;
             } else {
-                high = mid - 1;
+                right = mid - 1;
             }
         }
 
-        return ans;
+        return (int) right;
     }
 }
